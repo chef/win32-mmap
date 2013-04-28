71d6b891899186cd99795e105d68a3964723f1e1
Daniel Berger <djberg96@gmail.com>
2013-04-28 06:57:42 -0600

    Version bumps.

--- test/test_win32_mmap.rb
+++ test/test_win32_mmap.rb
@@ -14,7 +14,7 @@ class TC_Win32_Mmap < Test::Unit::TestCase
   end
 
   test "version is set to expected value" do
-    assert_equal('0.3.1', MMap::VERSION)
+    assert_equal('0.3.2', MMap::VERSION)
   end
 
   test "dynamic variable names and string values work as expected" do
